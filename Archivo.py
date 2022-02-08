c53c5a127e83ce924061275e70941967314d2392
IvanMelgar <99267385+IvanMelgar@users.noreply.github.com>
2022-02-08 16:47:26 +0100

    Create Archivo.py
    He añadido el dado.

--- Archivo.py
+++ Archivo.py
@@ -22,24 +22,22 @@ def menu():
         menu()
 
 def Hundir_la_flota():
-    tablero = {
-    0:[" J1 "," J2 "," J3 "," J4 "," J5 "," J6 "," J7 "," J8 "," J9 "," J10 "],
-    1:[" I1 "," I2 "," I3 "," I4 "," I5 "," I6 "," I7 "," I8 "," I9 "," I10 "],
-    2:[" H1 "," H2 "," H3 "," H4 "," H5 "," H6 "," H7 "," H8 "," H9 "," H10 "],
-    3:[" G1 "," G2 "," G3 "," G4 "," G5 "," G6 "," G7 "," G8 "," G9 "," G10 "],
-    4:[" F1 "," F2 "," F3 "," F4 "," F5 "," F6 "," F7 "," F8 "," F9 "," F10 "],
-    5:[" E1 "," E2 "," E3 "," E4 "," E5 "," E6 "," E7 "," E8 "," E9 "," E10 "],
-    6:[" D1 "," D2 "," D3 "," D4 "," D5 "," D6 "," D7 "," D8 "," D9 "," D10 "],
-    7:[" C1 "," C2 "," C3 "," C4 "," C5 "," C6 "," C7 "," C8 "," C9 "," C10 "],
-    8:[" B1 "," B2 "," B3 "," B4 "," B5 "," B6 "," B7 "," B8 "," B9 "," B10 "],
-    9:[" A1 "," A2 "," A3 "," A4 "," A5 "," A6 "," A7 "," A8 "," A9 "," A10 "],
-    #10:[" 1 "," 2 "," 3 "," 4 "," 5 "," 6 "," 7 "," 8 "," 9 "," 10"]
-    }
-
-    for x in range(len(tablero)):
-        print(tablero[x])
-def Oca():
     pass
+def Oca():
+    """import random
+       ju=int(input("Cuantos jugadores hay:"))
+       for i in range(ju):
+             x=input("Tirar dado SI/NO:")
+             if x=="si":
+             d1= random.randint(1,6)
+             d2=random.randint(1,6)
+             print ("El primer dado salio:",d1,"El segundo dado salio:",d2)
+             t= d1+d2
+             print("La suma de los dados es:",t)
+        elif x=="no":
+            print("Has decidido no tirar")
+        else: 
+            x=input("Tirar dado SI/NO:")"""
 def Escaleras_y_Serpientes():
     pass
 def Quien_es_quien():
@@ -50,17 +48,4 @@ def Conecta_4():
 menu()
 
 
-"""import random
-ju=int(input("Cuantos jugadores hay:"))
-for i in range(ju):
-    x=input("Tirar dado SI/NO:")
-    if x=="si":
-        d1= random.randint(1,6)
-        d2=random.randint(1,6)
-        print ("El primer dado salio:",d1,"El segundo dado salio:",d2)
-        t= d1+d2
-        print("La suma de los dados es:",t)
-    elif x=="no":
-        print("Has decidido no tirar")
-    else: 
-        x=input("Tirar dado SI/NO:")"""
+
