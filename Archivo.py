a3c6fd272e2c2a84d4708966747e641417441c7f
IvanMelgar <99267385+IvanMelgar@users.noreply.github.com>
2022-02-22 16:50:17 +0100

    Update Archivo.py
    hemos añadido los puestos de los jugadores de la oca

--- Archivo.py
+++ Archivo.py
@@ -357,40 +357,66 @@ def Hundir_la_flota():
     
     
 def Oca():
-    import random
-def Oca():
-    pos = 0
-    posi = 0
-    while pos!=64 or posi!=64:
+    tablero= {
+        8:["25","24","23","22","21","20","19","18","17","16"],
+        7:["26","51","50","49","48","47","46","45","44","15"],
+        6:["27","52","  ","  ","  ","  ","  ","  ","43","14"],
+        5:["28","53","  ","  ","  ","  ","  ","  ","42","13"],
+        4:["29","54","  ","  ","  ","  ","  ","62","41","12"],
+        3:["30","55","56","57","58","59","60","61","40","11"],
+        2:["31","32","33","34","35","36","37","38","39","10"],
+        1:["00","01","02","03","04","05","06","07","08","09"]
+    }
+    JU = 0
+    PC = 0
+    while JU!="" or PC!="":
         x=input("Tirar dado SI/NO:")
         if x=="si":
             d1= random.randint(1,6)
             d2=random.randint(1,6)
             t = d1+d2
-            pos = pos +t
-            print("El primer dado salio:",d1,"El segundo dado salio:",d2,"La suma de los dados es:",t,"Haz avanzado a la casilla :",pos)
-            d1 = random.randint(1, 6)
-            d2 = random.randint(1, 6)
-            t = d1 + d2
-            posi = posi + t
-            print("El primer dado salio:", d1, "El segundo dado salio:", d2,"La suma de los dados del PC es:", t,"El PC ha avanzado al puesto:",posi)
+            JU = JU +t
+            time.sleep(1)
+            print("El primer dado salio:",d1,",El segundo dado salio:",d2,",La suma de los dados es:",t,",Haz avanzado a la casilla :",JU)
+            dp1 = random.randint(1, 6)
+            dp2 = random.randint(1, 6)
+            to = dp1 + dp2
+            PC = PC + to
+            time.sleep(1)
+            print("El primer dado salio:", dp1, ",El segundo dado salio:", dp2,",La suma de los dados del PC es:", to,",El PC ha avanzado al puesto:",PC)
         elif x=="no":
             print("Has decidido no tirar")
         else:
             x=input("Tirar dado SI/NO:")
+        for y in tablero:
+            for z in range(len(tablero[y])):
+                if tablero[1][z]=="0"+str(JU):
+                    tablero[1][z]="JU"
+                else:
+                    if tablero[y][z] == str(JU):
+                        tablero[y][z] = "JU"
 
-    tablero= {
-        8:["25","24","23","22","21","20","19","18","17","16"],
-        7:["26","51","50","49","48","47","46","45","44","15"],
-        6:["27","52","64","64","64","64","64","64","43","14"],
-        5:["28","53","64","64","64","64","64","63","42","13"],
-        4:["29","54","64","64","64","64","64","62","41","12"],
-        3:["30","55","56","57","58","59","60","61","40","11"],
-        2:["31","32","33","34","35","36","37","38","39","10"],
-        1:["00","01","02","03","04","05","06","07","08","09"]
-    }
-    for x in tablero:
-        print(tablero[x])
+        for y in tablero:
+            for z in range(len(tablero[y])):
+                if tablero[1][z]=="0"+str(PC):
+                    tablero[1][z]="PC"
+                else:
+                    if tablero[y][z] == str(PC):
+                        tablero[y][z] = "PC"
+        for x in tablero:
+            time.sleep(0.1)
+            print(tablero[x])
+
+
+
+#def cespecial():
+#casillas_especiales=["01","05","09","14","18","23","27","32","36","41","45","50","54","59","63"]
+
+
+
+    #for x in tablero:
+        #print(tablero[x])
+Oca()
 def Escaleras_y_Serpientes():
     
         def Escaleras_y_Serpientes():
